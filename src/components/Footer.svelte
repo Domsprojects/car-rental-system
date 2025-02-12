7233324c172823f088791abe2644d20eb141f46e
Domsprojects <dspi.github@gmail.com>
2025-02-12 20:46:48 +0100

    Update Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -16,7 +16,7 @@
     <p
       class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4"
     >
-      © 2021 Dominika Spólnik
+      © 2021 Domsprojects
     </p>
     <span
       class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start"
